1fd80940def25b73a101d86335f2be15f53a4019
Pat Sissons <pat@marinels.com>
2015-10-21 14:17:33 -0700

    adding common events keys for pubsub

---
+++ src/Events.ts
@@ -0,0 +1,7 @@
+export class Events {
+  public static RoutingStateChanged = 'RoutingStateChanged';
+  public static RouteChanged = 'RouteChanged';
+  public static AlertCreated = 'AlertCreated';
+}
+
+export default Events;
