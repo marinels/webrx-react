da9e8084101168509917418bb572e3fce63ed6f0
Pat Sissons <patricksissons@gmail.com>
2015-10-22 11:31:47 -0700

    refactoring routing map and view map to their own files

---
+++ src/Components/App/RoutingMap.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+import { IRoutingMap } from '../RouteHandler/RouteHandlerViewModel';
+import { IRoute } from '../../Routing/RouteManager';
+
+import DashboardViewModel from '../Dashboard/DashboardViewModel';
+
+export let RoutingMap: IRoutingMap = {
+  '/': '/dashboard',
+  '/dashboard': (route?: IRoute) => new DashboardViewModel()
+};
+
+export default RoutingMap;
---
+++ src/Components/App/ViewMap.tsx
@@ -0,0 +1,14 @@
+'use strict';
+
+import * as React from 'react';
+
+import { IViewMap } from '../RouteHandler/RouteHandlerView';
+
+import DashboardViewModel from '../Dashboard/DashboardViewModel';
+import DashboardView from '../Dashboard/DashboardView';
+
+export let ViewMap: IViewMap = {
+  'DashboardViewModel': (viewModel: DashboardViewModel) => <DashboardView viewModel={viewModel} />
+};
+
+export default ViewMap;
