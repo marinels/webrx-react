c9a1033ccc445b659fd93513372b468738d4d952
Pat Sissons <pat@marinels.com>
2015-10-23 10:46:48 -0700

    using new bindable input component

--- src/Components/Dashboard/DashboardView.tsx
+++ src/Components/Dashboard/DashboardView.tsx
@@ -6,6 +6,7 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import { Input, Button } from 'react-bootstrap';
 
 import { BaseView, IBaseViewProps } from '../React/BaseView';
+import BindableInput from '../BindableInput/BindableInput';
 
 import DashboardViewModel from './DashboardViewModel';
 
@@ -26,15 +27,16 @@ export class DashboardView extends BaseView<IDashboardProps, DashboardViewModel>
       <Button disabled={this.state.generateAlert.canExecute(null) === false}
         onClick={this.bindEvent(x => x.generateAlert)}>Click to Generate an Alert</Button>
     );
-    
+
     return (
       <div className='Dashboard'>
         <Grid>
           <Row>
             <Col md={12}>
-              <Input groupClassName='AlertText' type='text' placeholder='Enter Alert Text...' bsSize='large'
-                defaultValue={this.state.alertText()} buttonAfter={generateButton} onChange={this.bindText(x => x.alertText)}
-                onKeyDown={this.bindEvent(x => x.generateAlert, (x: React.KeyboardEvent) => x.keyCode, x => x == 13)} />
+              <BindableInput property={this.state.alertText}>
+                <Input groupClassName='AlertText' type='text' placeholder='Enter Alert Text...' bsSize='large'
+                  buttonAfter={generateButton} onKeyDown={this.bindEvent(x => x.generateAlert, (x: React.KeyboardEvent) => x.keyCode, x => x == 13)} />
+              </BindableInput>
             </Col>
           </Row>
         </Grid>
