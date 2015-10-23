e41e5dd8b07e8787adb170f25a8bd5898e54031f
Pat Sissons <pat@marinels.com>
2015-10-23 10:36:43 -0700

    adding a new *ultra* efficient two-way bindable (text) input component.

---
+++ src/Components/BindableInput/BindableInput.tsx
@@ -0,0 +1,33 @@
+'use strict';
+
+import * as React from 'react';
+import * as wx from 'webrx';
+
+interface IBindableInputProps {
+  property: wx.IObservableProperty<string>;
+  //inputComponent: React.ReactElement<any>;
+  children?: React.ReactNode;
+  valueProperty?: string;
+  onChangeProperty?: string;
+}
+
+export class BindableInput extends React.Component<IBindableInputProps, any> {
+  render() {
+    let valueProp = this.props.valueProperty || 'value';
+
+    let onChange = (x: any) => {
+      this.props.property(x.target[valueProp].toString());
+      this.forceUpdate();
+    };
+
+    let props: { [key: string]: any} = {};
+    props[valueProp] = this.props.property();
+    props[this.props.onChangeProperty || 'onChange'] = onChange;
+
+    let inputComponent = React.Children.only(this.props.children) as React.ReactElement<any>;
+
+    return React.cloneElement(inputComponent, props);
+  }
+}
+
+export default BindableInput;
