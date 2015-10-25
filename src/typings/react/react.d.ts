73a8149f528ba7e9904a18b8e775d8ea23694545
Pat Sissons <psissons@acrotech.io>
2015-10-25 14:32:37 -0700

    fixing the react children map function typing

--- src/typings/react/react.d.ts
+++ src/typings/react/react.d.ts
@@ -773,7 +773,7 @@ declare namespace __React {
     // ----------------------------------------------------------------------
 
     interface ReactChildren {
-        map<T>(children: ReactNode, fn: (child: ReactChild, index: number) => T): { [key:string]: T };
+        map<T>(children: ReactNode, fn: (child: ReactChild, index: number) => T): T[];
         forEach(children: ReactNode, fn: (child: ReactChild, index: number) => any): void;
         count(children: ReactNode): number;
         only(children: ReactNode): ReactChild;
