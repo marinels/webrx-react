005e40530a0fbb5cb17667db1e0a65aaa6b7e1bb
Pat Sissons <pat@marinels.com>
2015-10-27 18:30:22 -0700

    search view model is now routable

--- src/Components/Search/SearchViewModel.ts
+++ src/Components/Search/SearchViewModel.ts
@@ -3,11 +3,39 @@
 import * as Rx from 'rx';
 import * as wx from 'webrx';
 
-import BaseViewModel from '../React/BaseViewModel';
+import BaseRoutableViewModel from '../React/BaseRoutableViewModel';
 
-export class SearchViewModel extends BaseViewModel {
+export interface ISearchRoutingState {
+  filter: string;
+}
+
+export class SearchViewModel extends BaseRoutableViewModel<ISearchRoutingState> {
+  constructor(isRoutingEnabled = false) {
+    super(isRoutingEnabled);
+  }
+
   public filter = wx.property('');
-  public search = wx.command(this.filter.changed.select(x => String.isNullOrEmpty(x) == false));
+  public search = wx.command();
+
+  initialize() {
+    this.subscribe(this.search.results.subscribe(x => {
+      this.routingStateChanged();
+    }));
+  }
+
+  getRoutingState() {
+    return this.createRoutingState(state => {
+      if (String.isNullOrEmpty(this.filter()) === false) {
+        state.filter = this.filter();
+      }
+    })
+  }
+
+  setRoutingState(state = {} as ISearchRoutingState) {
+    if (this.isRoutingEnabled) {
+      this.filter(state.filter || '');
+    }
+  }
 }
 
 export default SearchViewModel;
