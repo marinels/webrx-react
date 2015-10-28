07aff82793130778504f406bcc615add2a172303
Pat Sissons <pat@marinels.com>
2015-10-27 18:31:20 -0700

    updating pager component. now controls its own limit and offset. limit can now be modified from the UI. the pager is now routable.

--- src/Components/Pager/PagerView.tsx
+++ src/Components/Pager/PagerView.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 
-import { Pagination, PaginationProps } from 'react-bootstrap';
+import { Pagination, PaginationProps, DropdownButton, MenuItem } from 'react-bootstrap';
 
 import { BaseView, IBaseViewProps } from '../React/BaseView';
 
@@ -11,6 +11,8 @@ import PagerViewModel from './PagerViewModel';
 import './Pager.less';
 
 interface IPagerProps extends IBaseViewProps, PaginationProps {
+  info?: boolean;
+  limits?: number[];
 }
 
 export class PagerView extends BaseView<IPagerProps, PagerViewModel> {
@@ -22,13 +24,29 @@ export class PagerView extends BaseView<IPagerProps, PagerViewModel> {
   }
 
   render() {
+    let info: string;
+
+    if (this.props.info && this.state.hasValidLimit()) {
+      info = String.format('Showing Items {0} through {1}', this.state.offset() + 1, Math.min(this.state.itemCount(), this.state.offset() + this.state.limit()));
+    }
+
     let pager = this.state.pageCount() == null ? null : (
       <Pagination items={this.state.pageCount()} activePage={this.state.selectedPage()} onSelect={this.bindCallback(x => x.selectedPage, x => x[1].eventKey)} {...this.props} />
     );
 
+    let limit = this.props.limits == null ? null : (
+      <DropdownButton id='pager-limit-selector' title={String.format('Items per Page ({0})', this.state.limit() || 'All')} onSelect={this.bindCallback(x => x.limit, x => x[1])}>
+        {this.props.limits.map((x, i) => (
+          <MenuItem key={i} eventKey={x}>{x || 'All'}</MenuItem>
+        ))}
+      </DropdownButton>
+    );
+
     return (
       <div className='Pager'>
+        <div className='Pager-info pull-left'>{info}</div>
         {pager}
+        <div className='Pager-limit pull-right'>{limit}</div>
       </div>
     );
   }
--- src/Components/Pager/PagerViewModel.ts
+++ src/Components/Pager/PagerViewModel.ts
@@ -1,12 +1,67 @@
 'use strict';
 
+import * as Rx from 'rx';
 import * as wx from 'webrx';
 
-import BaseViewModel from '../React/BaseViewModel';
+import BaseRoutableViewModel from '../React/BaseRoutableViewModel';
 
-export class PagerViewModel extends BaseViewModel {
-  public pageCount = wx.property<number>();
+export interface IPagerRoutingState {
+  limit: number;
+  page: number;
+}
+
+export class PagerViewModel extends BaseRoutableViewModel<IPagerRoutingState> {
+  constructor(limit?: number, isRoutingEnabled = false) {
+    super(isRoutingEnabled);
+
+    this.limit(limit);
+  }
+
+  public itemCount = wx.property<number>();
+  public limit = wx.property<number>();
+  public pageCount = Rx.Observable
+    .combineLatest(this.itemCount.changed, this.limit.changed, (c, l) => Math.ceil(c / l))
+    .toProperty();
   public selectedPage = wx.property<number>();
+  public offset = this.selectedPage.changed
+    .select(x => (x - 1) * this.limit() || 0)
+    .toProperty();
+
+  initialize() {
+    this.pageCount.changed
+      .subscribe(x => {
+        this.selectedPage(1);
+      });
+
+    this.selectedPage.changed
+      .subscribe(x => {
+        this.routingStateChanged();
+      })
+  }
+
+  getRoutingState() {
+    return this.createRoutingState(state => {
+      if (this.hasValidLimit()) {
+        state.limit = this.limit();
+        state.page = this.selectedPage();
+      }
+    });
+  }
+
+  setRoutingState(state = {} as IPagerRoutingState) {
+    if (this.isRoutingEnabled) {
+      this.limit(state.limit);
+      this.selectedPage(state.page);
+    }
+  }
+
+  public hasValidLimit() {
+    return this.isValidLimit(this.limit());
+  }
+
+  private isValidLimit(limit: number) {
+    return limit != null && limit > 0;
+  }
 }
 
 export default PagerViewModel;
