68ad25db730eece741da2d87fea1e29cd6dba18a
Pat Sissons <pat@marinels.com>
2015-11-04 13:54:31 -0800

    adding npm packages for React animations (and typescript typings)

---
+++ src/typings/react-css-transition-replace/react-css-transition-replace.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="../react/react-addons-css-transition-group.d.ts"/>
+
+declare module 'react-css-transition-replace' {
+  var CSSTransitionReplace: __React.__Addons.CSSTransitionGroup;
+  type CSSTransitionReplace = __React.__Addons.CSSTransitionGroup;
+  export = CSSTransitionReplace;
+}
---
+++ src/typings/react/react-addons-css-transition-group.d.ts
@@ -0,0 +1,29 @@
+// Type definitions for React v0.14 (react-addons-css-transition-group)
+// Project: http://facebook.github.io/react/
+// Definitions by: Asana <https://asana.com>, AssureSign <http://www.assuresign.com>, Microsoft <https://microsoft.com>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="react.d.ts" />
+/// <reference path="react-addons-transition-group.d.ts" />
+
+declare namespace __React {
+    namespace __Addons {
+        interface CSSTransitionGroupProps extends TransitionGroupProps {
+            transitionName: string;
+            transitionAppear?: boolean;
+            transitionEnter?: boolean;
+            transitionLeave?: boolean;
+            transitionAppearTimeout?: number;
+            transitionEnterTimeout?: number;
+            transitionLeaveTimeout?: number;
+        }
+
+        type CSSTransitionGroup = ComponentClass<CSSTransitionGroupProps>;
+    }
+}
+
+declare module "react-addons-css-transition-group" {
+    var CSSTransitionGroup: __React.__Addons.CSSTransitionGroup;
+    type CSSTransitionGroup = __React.__Addons.CSSTransitionGroup;
+    export = CSSTransitionGroup;
+}
