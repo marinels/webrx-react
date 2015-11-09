289c5e99dbfc218b65eff678f047fe8c77912110
Pat Sissons <pat@marinels.com>
2015-11-09 14:15:30 -0800

    adding font awesome package

---
+++ src/typings/react-fa/react-fa.d.ts
@@ -0,0 +1,25 @@
+///<reference path="../react/react.d.ts"/>
+
+declare module 'react-fa' {
+  import React = require('react');
+
+  interface IconProps {
+    name: string;
+    className?: string;
+    size?: string;
+    rotate?: number;
+    flip?: string;
+    fixedWidth?: boolean;
+    spin?: boolean;
+    pulse?: boolean;
+    stack?: string;
+    inverse?: boolean;
+    Component?: string;
+  }
+
+  interface Icon extends React.ReactElement<IconProps> { }
+  interface IconClass extends React.ComponentClass<IconProps> { }
+  var Icon: IconClass;
+
+  export = Icon;
+}
