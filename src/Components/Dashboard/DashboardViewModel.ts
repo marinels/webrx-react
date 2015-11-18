cb510008f425aadd6ac802fb441481b8b3127da8
Pat Sissons <pat@marinels.com>
2015-11-18 14:28:52 -0800

    removing redundant constructor

--- src/Components/Dashboard/DashboardViewModel.ts
+++ src/Components/Dashboard/DashboardViewModel.ts
@@ -12,10 +12,6 @@ interface IDashboardRoutingState {
 export class DashboardViewModel extends BaseRoutableViewModel<IDashboardRoutingState> {
   public static displayName = 'DashboardViewModel';
 
-  constructor(isRoutingEnabled = false) {
-    super(isRoutingEnabled);
-  }
-
   public alertText = wx.property('');
 
   public generateAlert = wx.command(
