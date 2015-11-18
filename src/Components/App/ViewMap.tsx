0f8de95d397b476aed5de63525b176a789813faf
Pat Sissons <pat@marinels.com>
2015-11-17 18:00:30 -0800

    we don't need to use a string here

--- src/Components/App/ViewMap.tsx
+++ src/Components/App/ViewMap.tsx
@@ -8,7 +8,7 @@ import DashboardViewModel from '../Dashboard/DashboardViewModel';
 import DashboardView from '../Dashboard/DashboardView';
 
 export let ViewMap: IViewMap = {
-  'DashboardViewModel': (viewModel: DashboardViewModel) => <DashboardView viewModel={viewModel} />
+  DashboardViewModel: (viewModel: DashboardViewModel) => <DashboardView viewModel={viewModel} />
 };
 
 export default ViewMap;
