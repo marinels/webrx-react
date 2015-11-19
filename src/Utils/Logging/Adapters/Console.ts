cc32cc361c46941a29b6a25332039dbfa8c7f3dd
Pat Sissons <pat@marinels.com>
2015-11-19 11:15:17 -0800

    console logging now applies style based on level

--- src/Utils/Logging/Adapters/Console.ts
+++ src/Utils/Logging/Adapters/Console.ts
@@ -8,11 +8,48 @@ import DelegateLogManager from './Delegate';
 
 export class ConsoleLogManager extends DelegateLogManager {
   constructor(defaultLevel: LogLevel) {
-    super(this.logToConsole, defaultLevel);
+    super((action, level, text) => this.logAction(action, level, text), defaultLevel);
   }
 
-  private logToConsole(logger: ILogger, level: LogLevel, text: string) {
-    console.log(String.format('[{0}][{1}][{2}] {3}', moment().format('HH:mm:ss.SSS'), getLevelName(level), logger.name, text));
+  private getStyles(level: LogLevel) {
+    // these colors should never get used
+    let bgcolor = 'black';
+    let color = 'white'
+
+    if (level >= LogLevel.Fatal) {
+      bgcolor = 'darkred';
+      color = 'lightcoral';
+    } else if (level >= LogLevel.Error) {
+      bgcolor = 'lightcoral'
+      color = 'black';
+    } else if (level >= LogLevel.Warn) {
+      bgcolor = 'blanchedalmond ';
+      color = 'black';
+    } else if (level >= LogLevel.Info) {
+      bgcolor = 'lightblue ';
+      color = 'black';
+    } else if (level >= LogLevel.Debug) {
+      bgcolor = 'lightcyan';
+      color = 'black';
+    } else {
+      bgcolor = 'transparent';
+      color = 'black';
+    }
+
+    return [
+      String.format('background-color: {0}; color: {1}', bgcolor, color)
+    ];
+  }
+
+  private logAction(logger: ILogger, level: LogLevel, text: string) {
+    this.logToConsole(
+      String.format('%c[{0}][{1}][{2}] {3}', moment().format('HH:mm:ss.SSS'), getLevelName(level), logger.name, text),
+      ...this.getStyles(level)
+    );
+  }
+
+  private logToConsole(text: string, ...formatting: string[]) {
+    console.log(text, ...formatting);
   }
 }
 
