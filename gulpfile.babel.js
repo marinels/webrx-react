2958b5b8a8abd354f3b5f106d92bb571d6c3d95f
Pat Sissons <psissons@acrotech.io>
2015-12-10 21:36:04 -0800

    now using gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,249 @@
+'use strict';
+
+import fs from 'fs';
+import path from 'path';
+import del from 'del';
+import parseArgs from 'minimist';
+
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+import gopen from 'gulp-open';
+import greplace from 'gulp-replace';
+import grename from 'gulp-rename';
+import webpack from 'webpack';
+import WebpackDevServer from 'webpack-dev-server';
+import webpackConfig from './webpack.config.js'
+
+let args = parseArgs(process.argv);
+
+const config = {
+  dirs: {
+    src: 'src',
+    dest: args.dest || 'build'
+  },
+  filename: 'app.js',
+  host: '0.0.0.0',
+  port: 3000
+};
+
+const uri = 'http://' + (config.host === '0.0.0.0' ? 'localhost' : config.host) + ':' + config.port;
+
+function runWebpack(cfg, tag = 'default', callback) {
+  webpack(cfg).run((err, stats) => {
+    if (err) {
+      throw new gutil.PluginError('webpack:' + tag, err);
+    }
+
+    gutil.log('[webpack:' + tag + ']', stats.toString({
+			colors: true
+		}));
+
+    gutil.log('[webpack:' + tag + ']', cfg.output.filename);
+
+    if (callback) {
+      callback(stats);
+    }
+  })
+}
+
+function configureWebpack() {
+  let webpackConfigCopy = Object.create(webpackConfig);
+  webpackConfigCopy.output.path = config.dirs.dest;
+  webpackConfigCopy.output.filename = gutil.replaceExtension(config.filename, '.min.js');
+
+  webpackConfigCopy.plugins = [
+    new webpack.DefinePlugin({
+      DEBUG: false,
+      PRODUCTION: true,
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      comments: false,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en-ca|en-gb/)
+  ];
+
+  return webpackConfigCopy;
+}
+
+function writeStats(stats, filename, callback) {
+  fs.writeFileSync(path.join(__dirname, config.dirs.dest, filename), JSON.stringify(stats.toJson(), null, 2));
+
+  callback();
+}
+
+gulp.task('default', ['browser:webpack-dev-server']);
+
+gulp.task('clean', () => {
+  del([`${config.dirs.dest}/*`, 'npm-debug.log'])
+});
+
+gulp.task('build', ['webpack:build']);
+gulp.task('build:compat', ['webpack:build:compat']);
+gulp.task('build:dev', ['webpack:build:dev']);
+
+gulp.task('webpack:build', callback => {
+  let webpackConfigCopy = configureWebpack();
+
+  runWebpack(webpackConfigCopy, 'webpack:build', () => callback());
+});
+
+gulp.task('webpack:build:compat', callback => {
+  // let webpackConfigCopy = Object.create(webpackConfig);
+  // webpackConfigCopy.devtool = 'sourcemap';
+  // webpackConfigCopy.debug = true;
+
+  let webpackConfigCopy = configureWebpack();
+  webpackConfigCopy.output.filename = gutil.replaceExtension(config.filename, '.compat.min.js');
+  webpackConfigCopy.resolve.alias.rx = 'rx/dist/rx.all.compat';
+  webpackConfigCopy.ts = {
+    compilerOptions: {
+      target: 'es3'
+    }
+  }
+
+  webpackConfigCopy.entry.unshift('es5-shim', 'es5-shim/es5-sham');
+
+  runWebpack(webpackConfigCopy, 'webpack:build:compat', () => {
+
+    callback();
+  });
+});
+
+gulp.task('webpack:build:dev', callback => {
+  let webpackConfigCopy = Object.create(webpackConfig);
+  webpackConfigCopy.output.path = config.dirs.dest;
+  webpackConfigCopy.output.filename = config.filename;
+  webpackConfigCopy.devtool = 'sourcemap';
+  webpackConfigCopy.debug = true;
+
+  runWebpack(webpackConfigCopy, 'webpack:build:dev', () => callback());
+});
+
+gulp.task('stats', ['webpack:stats']);
+gulp.task('stats:dev', ['webpack:stats:dev']);
+
+gulp.task('webpack:stats', callback => {
+  let webpackConfigCopy = configureWebpack();
+  webpackConfigCopy.failOnError = false;
+  webpackConfigCopy.profile = true;
+
+  runWebpack(webpackConfigCopy, 'webpack:stats', stats => {
+    writeStats(stats, 'stats.min.json', callback);
+  });
+});
+
+gulp.task('webpack:stats:dev', callback => {
+  let webpackConfigCopy = Object.create(webpackConfig);
+  webpackConfigCopy.devtool = 'sourcemap';
+  webpackConfigCopy.debug = true;
+  webpackConfigCopy.failOnError = false;
+  webpackConfigCopy.profile = true;
+
+  runWebpack(webpackConfigCopy, 'webpack:stats-dev', stats => {
+    writeStats(stats, 'stats.json', callback);
+  });
+});
+
+gulp.task('server', ['webpack-dev-server']);
+
+gulp.task('webpack-dev-server', callback => {
+  let webpackConfigCopy = Object.create(webpackConfig);
+  webpackConfigCopy.devtool = 'eval';
+  webpackConfigCopy.debug = true;
+
+  webpackConfigCopy.entry.unshift('webpack-dev-server/client?' + uri, 'webpack/hot/only-dev-server');
+
+  webpackConfigCopy.plugins = [
+    new webpack.DefinePlugin({
+      DEBUG: true,
+      PRODUCTION: false
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+
+  ];
+
+  let compiler = webpack(webpackConfigCopy);
+  compiler.plugin('done', stats => {
+    gutil.log('[webpack-dev-server]', 'Listening at ' + config.host + ':' + config.port);
+    gutil.log('[webpack-dev-server]', uri + '/webpack-dev-server/index.html');
+
+    callback();
+  });
+
+  new WebpackDevServer(compiler, {
+    publicPath: webpackConfigCopy.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    stats: {
+      colors: true
+    }
+  }).listen(config.port, config.host, (err, result) => {
+    if (err) {
+      throw new gutil.PluginError('webpack-dev-server', err);
+    }
+  });
+});
+
+gulp.task('index', ['index:build']);
+gulp.task('index:compat', ['index:build:compat']);
+gulp.task('index:dev', ['index:build:dev']);
+
+gulp.task('index:build', ['webpack:build'], () => {
+  gulp
+    .src(path.join(__dirname, 'index.html'))
+    .pipe(greplace(/app\.js/g, 'app.min.js'))
+    .pipe(gulp.dest(path.join(__dirname, config.dirs.dest, 'index.min.html')));
+});
+
+gulp.task('index:build:compat', ['webpack:build:compat'], () => {
+  gulp
+    .src(path.join(__dirname, 'index.html'))
+    .pipe(greplace('app.js', 'app.compat.min.js'))
+    .pipe(grename('index.compat.min.html'))
+    .pipe(gulp.dest(path.join(__dirname, config.dirs.dest)));
+});
+
+gulp.task('index:build:dev', ['webpack:build:dev'], () => {
+  gulp
+    .src(path.join(__dirname, 'index.html'))
+    .pipe(gulp.dest(path.join(__dirname, config.dirs.dest)));
+});
+
+gulp.task('browser', ['browser:build']);
+gulp.task('browser:compat', ['browser:build:compat']);
+gulp.task('browser:dev', ['browser:build:dev']);
+
+gulp.task('browser:build', ['index:build'], () => {
+  let uri = path.join(__dirname, config.dirs.dest, 'index.min.html');
+  gulp
+    .src('')
+    .pipe(gopen({ uri }));
+});
+
+gulp.task('browser:build:compat', ['index:build:compat'], () => {
+  let uri = path.join(__dirname, config.dirs.dest, 'index.compat.min.html');
+  gulp
+    .src('')
+    .pipe(gopen({ uri }));
+});
+
+gulp.task('browser:build:dev', ['index:build:dev'], () => {
+  let uri = path.join(__dirname, config.dirs.dest, 'index.html');
+  gulp
+    .src('')
+    .pipe(gopen({ uri }));
+});
+
+gulp.task('browser:webpack-dev-server', ['webpack-dev-server'], () => {
+  gulp
+    .src('')
+    .pipe(gopen({ uri }));
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,18 @@
 var path = require('path');
 var webpack = require('webpack');
 
-var defines = {
-  DEBUG: false,
-  PRODUCTION: false
-};
-var plugins = [];
-var output = 'build';
-var fileName = 'app.js';
-var entry = [
-  // 'es5-shim',
-  // 'es5-shim/es5-sham',
-  './src/index.tsx',
-  './index.html'
-];
-var aliases = {
-  Ix: 'ix/l2o', // the ix package uses Ix to refer to l2o for some reason
-  // rx: 'rx/dist/rx.all.compat'
-};
-
-var isProduction = process.argv.indexOf('-p') >= 0 ? true : false;
-
-if (isProduction) {
-  defines.PRODUCTION = true;
-  fileName = 'app.min.js';
-
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      comments: false,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en-ca|en-gb/)
-  );
-} else {
-  defines.DEBUG = true;
-}
-
-plugins.push(
-  new webpack.DefinePlugin(defines)
-)
-
 module.exports = {
-  entry: entry,
+  entry: ['./src/index.tsx'],
   output: {
-    path: path.join(__dirname, output),
-    filename: fileName
+    path: path.join(__dirname, 'build'),
+    filename: 'app.js'
   },
-  devtool: '#source-map',
-  plugins: plugins,
+  plugins: [
+    new webpack.DefinePlugin({
+      DEBUG: false,
+      PRODUCTION: false
+    })
+  ],
   module: {
     loaders: [
       { test: /\.css$/, loader: 'style!css' },
@@ -59,24 +20,14 @@ module.exports = {
       //{ test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file?name=[name].[ext]" },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url?mimetype=application/font-woff" },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url" },
-      { test: /\.tsx?$/, loaders: ['react-hot', 'ts'], include: path.join(__dirname, 'src') },
-      { test: /index\.html$/, loader: 'file?name=[name].[ext]' }
+      { test: /\.tsx?$/, loaders: ['react-hot', 'ts'], include: path.join(__dirname, 'src') }
     ]
   },
   resolve: {
     extensions: ['', '.ts', '.tsx', '.webpack.js', '.web.js', '.js'],
-    alias: aliases
-  },
-  stats: {
-    reasons: true,
-    chunkModules: true,
-    chunkOrigins: true,
-    modules: true,
-    cached: true,
-    cachedAssets: true,
-    source: true,
-    errorDetails: true,
-    publicPath: true
+    alias: {
+      Ix: 'ix/l2o', // the ix package uses Ix to refer to l2o for some reason
+    }
   },
   progress: true,
   failOnError: true
