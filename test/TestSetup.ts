e527e08c923526013c20cb196c4926c1d1c7c9ca
Pat Sissons <pat@marinels.com>
2015-12-21 11:34:32 -0800

    updating test setup to support console tests

--- test/TestSetup.ts
+++ test/TestSetup.ts
@@ -1,7 +1,54 @@
 'use strict';
 
-// WebRx calls this when in a unit test context
-// this is a fake (empty) result that won't interfere
-(window as {[key: string]: any})['createMockHistory'] = () => {
-  return { onPopState: Rx.Observable.never<PopStateEvent>() };
-};
+declare var global: any;
+
+import * as Rx from 'rx';
+
+// WebRx assumes a lot about the state of javascript.
+// This setup will mock the assumptions so that we can continue testing its
+// components without crashing.
+function setupWebRx() {
+  if (global.window == null) {
+    // fake window for WebRx
+    global.window = {
+      navigator: {
+        userAgent: 'Mocha'
+      }
+    };
+  }
+
+  // fake document for WebRx
+  if (global.document == null) {
+    global.document = {
+      createElement: () => {
+        return {
+          getElementsByTagName: () => {
+            return {
+              iElems: [false]
+            }
+          }
+        }
+      },
+      implementation: {
+        createHTMLDocument: () => {
+          return {};
+        }
+      }
+    };
+  }
+
+  // fake jasmine for WebRx
+  global.window.jasmine = {
+    version_: 'Mocha'
+  };
+
+  // WebRx calls this when in a unit test context
+  // this is a fake (empty) result that won't interfere
+  global.window.createMockHistory = () => {
+    return {
+      onPopState: Rx.Observable.never<PopStateEvent>()
+    };
+  };
+}
+
+setupWebRx();
