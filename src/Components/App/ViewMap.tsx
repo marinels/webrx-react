4d66ba635a9cfe91d4bbedca6e136e1137d8ea93
Pat Sissons <pat@marinels.com>
2015-12-22 11:27:58 -0800

    stubbing in routing for component demo framework (commented out)

--- src/Components/App/ViewMap.tsx
+++ src/Components/App/ViewMap.tsx
@@ -7,8 +7,12 @@ import { IViewMap } from '../Common/RouteHandler/RouteHandlerView';
 import DashboardViewModel from '../Dashboard/DashboardViewModel';
 import DashboardView from '../Dashboard/DashboardView';
 
+// import ComponentDemoViewModel from '../Demo/ComponentDemoViewModel';
+// import ComponentDemoView from '../Demo/ComponentDemoView';
+
 export let ViewMap: IViewMap = {
-  DashboardViewModel: (viewModel: DashboardViewModel) => <DashboardView viewModel={viewModel} />
+  DashboardViewModel: (viewModel: DashboardViewModel) => <DashboardView viewModel={viewModel} />,
+  // ComponentDemoViewModel: (viewModel: ComponentDemoViewModel) => <ComponentDemoView viewModel={viewModel} />
 };
 
 export default ViewMap;
