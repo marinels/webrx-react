6c45c7acca07c4cf1f5740f3eb5db7d5d6c1be94
Pat Sissons <pat@marinels.com>
2016-02-26 13:33:00 -0800

    stringify is now much better at rendering structures

--- src/Extensions/String.ts
+++ src/Extensions/String.ts
@@ -9,10 +9,23 @@ function isNullOrEmpty(value: string) {
 }
 
 function stringify(value: any, replacer?: any, space: string | number = 2) {
-  let result = value == null ? null : value.toString();
+  let result: string = null;
 
-  if (result === '[object Object]') {
-    result = JSON.stringify(value, replacer, space);
+  if (value != null) {
+    result = value.toString();
+
+    if (result === '[object Object]') {
+      result = Object.getName(value);
+
+      if (result === 'Object') {
+        try {
+          result = JSON.stringify(value, replacer, space);
+        } catch (e) {
+          console.warn('Attempt to stringify failed');
+          console.log(e);
+        }
+      }
+    }
   }
 
   return result;
