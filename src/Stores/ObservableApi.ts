c34c927cd1455333bda0758f6c1e804f187f77e7
Pat Sissons <pat@marinels.com>
2016-03-02 15:17:01 -0800

    observable api will now use sample data if the subclass defines it

--- src/Stores/ObservableApi.ts
+++ src/Stores/ObservableApi.ts
@@ -4,11 +4,13 @@ import * as Rx from 'rx';
 import * as wx from 'webrx';
 
 import logManager from '../Components/Common/App/Logging';
+import SampleData from './SampleData/SampleData';
 
 export class ObservableApi {
   public static displayName = 'ObservableApi';
 
   protected logger = logManager.getLogger(Object.getName(this));
+  protected sampleData: SampleData = null;
 
   constructor(public baseUri: string) {
   }
@@ -20,24 +22,27 @@ export class ObservableApi {
 
     this.logger.debug('Calling API: {0} ({1})', action, uri);
 
-    // if an API call throws an uncaught error, that means you are not subscribing to the observable's error
-    return Rx.Observable
-      .fromPromise(this.client.get<T>(uri, params, options))
-      .catch(x => {
-        this.logger.error('API ERROR: {0} ({1})', action, uri);
-        this.logger.error(JSON.stringify(x, null, 2));
+    return this.sampleData == null ?
+      // if an API call throws an uncaught error, that means you are not subscribing to the observable's error
+      Rx.Observable
+        .fromPromise(this.client.get<T>(uri, params, options))
+        .catch(x => {
+          this.logger.error('API ERROR: {0} ({1})', action, uri);
+          this.logger.error(JSON.stringify(x, null, 2));
 
-        let error: any = null;
+          let error: any = null;
 
-        try {
-          error = JSON.parse(x.response);
-        } catch (e) {
-        }
+          try {
+            error = JSON.parse(x.response);
+          } catch (e) {
+          }
 
-        x.error = error;
+          x.error = error;
 
-        return Rx.Observable.throw<T>(x);
-      });
+          return Rx.Observable.throw<T>(x);
+        }) :
+        // if sample data has been created just use that instead (opt-in)
+        this.sampleData.getObservable<T>(action, params);
   }
 }
 
