2de78abd288687a6d26acbdb2eb00cb0c64ffc4c
Pat Sissons <pat@marinels.com>
2016-05-06 15:04:18 -0700

    updating the react-bootstrap typings, now supports updated form controls components

--- src/typings/react-bootstrap-overrides/react-bootstrap-overrides.d.ts
+++ src/typings/react-bootstrap-overrides/react-bootstrap-overrides.d.ts
@@ -1,27 +1,83 @@
-/// <reference path="../../../typings/main/ambient/react/react.d.ts" />
+/// <reference path="../../../typings/main/ambient/react/index.d.ts" />
+
 // tslint:disable:no-unused-variable
 
+// The react-bootstrap typing are a bit out of date
+// these overrides will fill in the missing components
+
 declare module 'react-bootstrap' {
-  interface ITitleProp {
-    // title renders as a tooltip
-    title?: string;
+  import React = require('react');
+
+  interface FormGroupProps extends React.HTMLAttributes {
+    bsClass?: string;
+    bsSize?: string;
+    controlId?: string;
+    validationState?: string;
+  }
+  class FormGroup extends __React.Component<FormGroupProps, any> { }
+
+  interface FormControlProps extends React.HTMLAttributes {
+    bsClass?: string;
+    componentClass?: string;
+    id?: string;
+    type?: string;
+  }
+  interface FormControlFeedbackProps extends React.HTMLAttributes {
+    bsClass?: string;
+  }
+  class FormControlFeedback extends __React.Component<FormControlFeedbackProps, any> { }
+  class FormControl extends __React.Component<FormControlProps, any> {
+    static Feedback: FormControlFeedback;
   }
 
-  interface InputProps extends ITitleProp {
-    // If you don't want the form-group class applied apply the prop named standalone
-    // see: https://react-bootstrap.github.io/components.html#forms
-    standalone?: boolean;
+  interface ControlLabelProps extends React.HTMLAttributes {
+    bsClass?: string;
+    htmlFor?: string;
+    srOnly?: boolean;
+  }
+  class ControlLabel extends React.Component<ControlLabelProps, any> { }
 
-    // handling of unfocus events
-    onBlur?: __React.FocusEventHandler;
+  interface CheckboxProps extends React.HTMLAttributes {
+    bsClass?: string;
+    disabled?: boolean;
+    inline?: boolean;
+    validationState?: string;
+  }
+  class Checkbox extends React.Component<CheckboxProps, any> { }
+
+  interface RadioProps extends React.HTMLAttributes {
+    bsClass?: string;
+    disabled?: boolean;
+    inline?: boolean;
+    validationState?: string;
+  }
+  class Radio extends React.Component<RadioProps, any> { }
+
+  interface HelpBlockProps extends React.HTMLAttributes {
+    bsClass?: string;
+  }
+  class HelpBlock extends React.Component<HelpBlockProps, any> { }
+
+  interface FormProps extends React.HTMLAttributes {
+    bsClass?: string;
+    componentClass?: string;
+    horizontal?: boolean;
+    inline?: boolean;
+  }
+  class Form extends React.Component<FormProps, any> { }
+
+  interface InputGroupProps extends React.HTMLAttributes {
+    bsClass?: string;
+    bsSize?: string;
+  }
+  class InputGroupAddon extends React.Component<React.HTMLAttributes, any> { }
+  class InputGroupButton extends React.Component<React.HTMLAttributes, any> { }
+  class InputGroup extends React.Component<InputGroupProps, any> {
+    static Addon: InputGroupAddon;
+    static Button: InputGroupButton;
   }
 
-  interface LabelProps extends ITitleProp {
-  }
-
-  // <Image />
-  // this component is missing entirely from the typings
-  interface ImageProps extends __React.Props<ImageClass> {
+  interface ImageProps extends React.HTMLAttributes {
     src: string;
     responsive?: boolean;
     rounded?: boolean;
@@ -31,7 +87,5 @@ declare module 'react-bootstrap' {
     width?: number;
     height?: number;
   }
-  interface Image extends __React.ReactElement<ImageProps> { }
-  interface ImageClass extends __React.ComponentClass<ImageProps> { }
-  let Image: ImageClass;
+  class Image extends React.Component<ImageProps, any> { }
 }
