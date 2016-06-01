a36ce45e954d4e7c349a37d2523a4a42056b0a4b
Pat Sissons <pat@marinels.com>
2016-06-01 12:52:29 -0700

    adding the sidebar component

---
+++ src/Components/Common/PageHeader/Sidebar.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+import { Modal } from 'react-bootstrap';
+
+import './Sidebar.less';
+
+interface SidebarProps {
+  onHide: Function;
+  isVisible?: boolean;
+  side?: string;
+  header?: any;
+  children?: any;
+}
+
+export class Sidebar extends React.Component<SidebarProps, {}> {
+  public static displayName = 'Sidebar';
+
+  static defaultProps = {
+    side: 'left',
+  };
+
+  render() {
+    return (
+      <Modal className={`Sidebar ${this.props.side}`}
+        onHide={this.props.onHide} show={this.props.isVisible}
+        autoFocus keyboard
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>{this.props.header}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          { this.props.children }
+        </Modal.Body>
+      </Modal>
+    );
+  }
+}
