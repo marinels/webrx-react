0abdbd93922eff0162bccfaab1eb7b937689e982
Pat Sissons <pat@marinels.com>
2016-11-04 12:03:03 -0700

    upgrading the async data grid to better support a more realistic data source pattern

--- src/Components/Common/DataGrid/AsyncDataGridViewModel.ts
+++ src/Components/Common/DataGrid/AsyncDataGridViewModel.ts
@@ -3,23 +3,35 @@ import { Observable } from 'rx';
 import { DataGridViewModel } from './DataGridViewModel';
 import { SortDirection } from '../../../Utils/Compare';
 
+export interface AsyncDataRequest {
+  filter: string;
+  offset: number;
+  limit: number;
+  sortField?: string;
+  sortDirection?: SortDirection;
+}
+
+export interface AsyncDataResult<TData> {
+  data: TData[];
+  count: number;
+}
+
 export interface AsyncDataSource<TData> {
-  getCount(filter: string): Observable<number>;
-  getResults(filter: string, offset: number, limit: number, sortField?: string, sortDirection?: SortDirection): Observable<TData[]>;
+  getResultAsync(request: AsyncDataRequest): Observable<AsyncDataResult<TData>>;
 }
 
 export class AsyncDataGridViewModel<TData> extends DataGridViewModel<TData> {
   public static displayName = 'AsyncDataGridViewModel';
 
   constructor(
-    private dataSource: AsyncDataSource<TData>,
+    protected dataSource: AsyncDataSource<TData>,
     protected enableFilter = false,
     protected enableSort = false,
     isMultiSelectEnabled?: boolean,
     rateLimit?: number,
     isRoutingEnabled?: boolean
   ) {
-    super(null, null, null, isMultiSelectEnabled, rateLimit, isRoutingEnabled);
+    super(undefined, undefined, undefined, isMultiSelectEnabled, rateLimit, isRoutingEnabled);
   }
 
   canFilter() {
@@ -31,13 +43,30 @@ export class AsyncDataGridViewModel<TData> extends DataGridViewModel<TData> {
   }
 
   projectItems() {
-    this.dataSource
-      .getCount(this.search.filter() || '')
-      .subscribe(x => {
-        this.pager.itemCount(x);
-      });
-
     return this.dataSource
-      .getResults(this.search.filter() || '', this.pager.offset() || 0, this.pager.limit() || 0, this.sortField(), this.sortDirection());
+      .getResultAsync(this.getRequestParams())
+      .doOnNext(x => {
+        this.pager.itemCount(x.count);
+      })
+      .doOnError(e => {
+        this.alertForError(e, 'Async DataSource Error');
+      })
+      .map(x => x.data);
+  }
+
+  protected getRequestParams(
+    filter = this.search.filter() || '',
+    offset = this.pager.offset() || 0,
+    limit = this.pager.limit() || 0,
+    sortField = this.sortField(),
+    sortDirection = this.sortDirection()
+  ) {
+    return <AsyncDataRequest>{
+      filter,
+      offset,
+      limit,
+      sortField,
+      sortDirection,
+    };
   }
 }
