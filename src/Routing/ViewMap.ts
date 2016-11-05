2b73dbbcf01191fff779f3ef719b1b43142d8a64
Pat Sissons <patricksissons@gmail.com>
2016-11-05 16:36:30 -0700

    cleaning up left over comments in the view map

--- src/Routing/ViewMap.ts
+++ src/Routing/ViewMap.ts
@@ -1,12 +1,6 @@
-// import * as React from 'react';
-
-// import { ViewMapper } from '../RouteHandler/RouteHandlerView';
-// import { Splash } from '../Splash/Splash';
-
 export interface ViewMapper {
   [key: string]: any;
 }
 
 export const ViewMap = {
-  // Splash: () => <Splash header='WebRx.React' />,
 } as ViewMapper;
