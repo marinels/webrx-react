6ef64d75f0d2fdaa8671d5a512d546473f479cf4
Pat Sissons <patricksissons@gmail.com>
2016-11-11 01:53:43 -0800

    updating the inline edit control to be more versatile

--- src/Components/Common/InlineEdit/InlineEditView.tsx
+++ src/Components/Common/InlineEdit/InlineEditView.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { Icon } from 'react-fa';
-import { FormGroup, InputGroup, FormControl } from 'react-bootstrap';
+import * as classNames from 'classnames';
+import { FormGroup, InputGroup, FormControl, Sizes } from 'react-bootstrap';
 
 import { BaseView, BaseViewProps } from '../../React/BaseView';
 import { BindableInput } from '../BindableInput/BindableInput';
@@ -9,63 +11,101 @@ import { InlineEditViewModel } from './InlineEditViewModel';
 
 import './InlineEdit.less';
 
-interface InlineEditProps extends BaseViewProps {
-  valueSelector?: (x: string) => string;
-  callBack?: (x: string) => any;
-  inputType?: string;
+interface InlineEditProps<T> extends BaseViewProps {
+  template?: (x: T, view: InlineEditView) => any;
+  editTemplate?: (x: T, view: InlineEditView) => any;
+  bsSize?: Sizes;
+  controlId?: string;
 }
 
-export class InlineEdit extends BaseView<InlineEditProps, InlineEditViewModel> {
-  public static displayName = 'InlineEdit';
+export class InlineEditView extends BaseView<InlineEditProps<any>, InlineEditViewModel<any>> {
+  public static displayName = 'InlineEditView';
+
+  static defaultProps = {
+    template: (x: any, view: InlineEditView) => x.toString(),
+    editTemplate: (x: any, view: InlineEditView) => (
+      <FormControl ref='control' type='text' placeholder='Enter New Value...'
+        onKeyDown={ e => view.handleKeyDown(e) }
+      />
+    ),
+  };
+
+  private handleKeyDown(e: React.KeyboardEvent) {
+    switch (e.keyCode) {
+      case 13: // ENTER key
+        this.state.save.execute(null);
+        break;
+      case 27: // ESC key
+        this.state.cancel.execute(null);
+        break;
+      default:
+        break;
+    }
+  }
+
+  private focusAndSelectControlText() {
+    const control = ReactDOM.findDOMNode(this.refs['control']) as HTMLInputElement;
+
+    if (control != null) {
+      control.focus();
+
+      if (control.select != null) {
+        control.select();
+      }
+    }
+  }
 
   updateOn() {
     return [
-      this.state.isInEditMode.changed,
+      this.state.isEditing.changed,
     ];
   }
 
+  updated(prevProps: InlineEditProps<any>) {
+    super.updated(prevProps);
+
+    this.focusAndSelectControlText();
+  }
+
   render() {
+    return this.renderConditional(this.state.isEditing, () => this.renderEditor(), () => this.renderValue());
+  }
+
+  private renderEditor() {
+    const { className, rest } = this.restProps(x => {
+      const { template, editTemplate } = x;
+      return { template, editTemplate };
+    });
+
     return (
-      <div className='InlineEdit'>
-        {
-          this.renderConditional(this.state.isInEditMode,
-          () => (
-            <FormGroup>
-              <InputGroup>
-                <BindableInput property={ this.state.val } >
-                  <FormControl type={ this.getType() } placeholder='Enter new result...' />
-                </BindableInput>
-                <CommandButton className='UpdateButton' bsStyle='success' command={ this.state.update } commandParameter={ () => this.props.callBack }>
-                  <Icon name='check' size='lg' />
-                </CommandButton>
-                <CommandButton className='CancelButton' bsStyle='danger' command={ this.state.update }>
-                  <Icon name='times' size='lg' />
-                </CommandButton>
-              </InputGroup>
-            </FormGroup>
-          )
-          ,() => (
-            <p onClick={ this.bindEventToCommand(x => x.changeMode) }>
-              <span>{ this.loadStr(this.props.valueSelector, this.state.val) }</span>
-            </p>
-          )
-        )
-        }
-      </div>
+      <FormGroup { ...rest } className={ classNames('InlineEditView', className)}>
+        <InputGroup>
+          <BindableInput property={ this.state.editValue } >
+            { this.props.editTemplate(this.state.editValue(), this) }
+          </BindableInput>
+          <InputGroup.Button>
+            <CommandButton bsSize={ this.props.bsSize } bsStyle='success' command={ this.state.save }>
+              <Icon name='check' />
+            </CommandButton>
+            <CommandButton bsSize={ this.props.bsSize } bsStyle='danger' command={ this.state.cancel }>
+              <Icon name='times' />
+            </CommandButton>
+          </InputGroup.Button>
+        </InputGroup>
+      </FormGroup>
     );
   }
 
-  private getType(): string {
-    return (typeof this.props.inputType === 'string')
-      ? this.props.inputType
-      : 'text';
-  }
+  private renderValue() {
+    const { className, rest } = this.restProps(x => {
+      const { template, editTemplate, controlId } = x;
+      return { template, editTemplate, controlId };
+    });
 
-  private loadStr(fn: Function, val: wx.IObservableProperty<any>) {
-    let strVal = String(val());
-    if (typeof fn !== 'function') {
-      return strVal;
-    }
-    return fn(strVal);
+    return (
+      <CommandButton { ...rest } className={ classNames('InlineEditView', className)} bsStyle='link' command={ this.state.edit }>
+        <span>{ this.props.template(this.state.value(), this) }</span>
+      </CommandButton>
+    );
   }
 }
--- src/Components/Common/InlineEdit/InlineEditViewModel.ts
+++ src/Components/Common/InlineEdit/InlineEditViewModel.ts
@@ -1,30 +1,51 @@
+import { Observable } from 'rx';
 import * as wx from 'webrx';
 
 import { BaseViewModel } from '../../React/BaseViewModel';
 
-export class InlineEditViewModel extends BaseViewModel {
+export class InlineEditViewModel<T> extends BaseViewModel {
   public static displayName = 'InlineEditViewModel';
 
-  public val: wx.IObservableProperty<number>;
-  public changeMode: wx.ICommand<any>;
-  public update: wx.ICommand<any>;
-  public isInEditMode: wx.IObservableProperty<boolean>;
+  public value: wx.IObservableProperty<T>;
+  public editValue: wx.IObservableProperty<T>;
+  public isEditing: wx.IObservableReadOnlyProperty<boolean>;
 
-  constructor(val: wx.IObservableProperty<any>) {
+  public edit: wx.ICommand<T>;
+  public save: wx.ICommand<T>;
+  public cancel: wx.ICommand<T>;
+
+  constructor(value?: wx.IObservableProperty<T> | T) {
     super();
 
-    this.isInEditMode = wx.property(false);
-    this.val = val;
+    if (wx.isProperty(value) === true) {
+      this.value = <wx.IObservableProperty<T>>value;
+    }
+    else {
+      this.value = wx.property(<T>value);
+    }
 
-    this.changeMode = wx.command(() => {
-      this.isInEditMode(true);
+    this.editValue = wx.property<T>();
+
+    this.edit = wx.asyncCommand(() => {
+      this.editValue(this.value());
+
+      return Observable.of(this.editValue());
+    });
+    this.save = wx.asyncCommand(() => {
+      this.value(this.editValue());
+      this.editValue(null);
+
+      return Observable.of(this.value());
+    });
+    this.cancel = wx.asyncCommand<T>(() => {
+      this.editValue(null);
+
+      return Observable.of(this.editValue());
     });
 
-    this.update = wx.command((callBackFn) => {
-      if (callBackFn instanceof Function) {
-        callBackFn(this.val());
-      }
-      this.isInEditMode(false);
-    });
+    this.isEditing = wx
+      .whenAny(this.editValue, x => x)
+      .map(x => x != null)
+      .toProperty();
   }
 }
