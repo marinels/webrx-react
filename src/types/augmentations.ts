21cdc13156fbfa0f71027c4fb28ad6d781837f53
Pat Sissons <pat@marinels.com>
2016-11-28 14:23:23 -0800

    removing obsoleted custom typings (now in @types)

--- src/types/augmentations.ts
+++ src/types/augmentations.ts
@@ -1,22 +1,3 @@
 // tslint:disable:no-unused-variable
 
-import { HTMLProps, ClassicComponent, ClassicComponentClass } from 'react';
-
-declare module 'react-bootstrap' {
-  // injecting fill prop for <Table />
-  interface TableProps {
-    fill?: boolean;
-  }
-
-  // add missing <Navbar.Form />
-  interface NavbarFormProps extends HTMLProps<NavbarForm> {
-    componentClass?: any;
-    pullRight?: boolean;
-  }
-  type NavbarForm = ClassicComponent<NavbarFormProps, {}>;
-  let NavbarForm: ClassicComponentClass<NavbarFormProps>;
-
-  interface NavbarClass {
-    Form: typeof NavbarForm;
-  }
-}
+// currently there are no augmenatations
