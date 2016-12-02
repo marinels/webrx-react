3496e7d2a56d5eb1c4bc0f0d291b5e7422693be1
Pat Sissons <pat@marinels.com>
2016-12-02 13:27:08 -0800

    adding invokeCommand patch

--- src/Extensions/WebRx.ts
+++ src/Extensions/WebRx.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rx';
 import * as wx from 'webrx';
 
 declare module 'webrx' {
@@ -28,3 +29,20 @@ function wrapCommand<T extends Function>(func: T, thisArg?: any) {
 
 (<any>wx).command = wrapCommand(wx.command);
 (<any>wx).asyncCommand = wrapCommand(wx.asyncCommand);
+
+// this is a patch for invokeCommand to support command selector parameters
+function invokeCommand<T, TResult>(command: (x: T) => wx.ICommand<TResult> | wx.ICommand<TResult>) {
+  // see the ReactiveUI project for the inspiration behind this function:
+  // https://github.com/reactiveui/ReactiveUI/blob/master/ReactiveUI/ReactiveCommand.cs#L524
+  return (this as Observable<T>)
+    .select(x => ({
+      parameter: x,
+      command: <wx.ICommand<TResult>>(command instanceof Function ? command(x) : command),
+    }))
+    .debounce(x => x.command.canExecuteObservable.startWith(x.command.canExecute(x.parameter)).where(b => b))
+    .select(x => x.command.executeAsync(x.parameter).catch(Rx.Observable.empty<TResult>()))
+    .switch()
+    .subscribe();
+}
+
+(<any>Observable).prototype.invokeCommand = invokeCommand;
