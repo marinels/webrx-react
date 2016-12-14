226253c95b25f0d2d3924c7ef760934c47817ee3
Pat Sissons <pat@marinels.com>
2016-12-13 16:37:29 -0800

    updating search component to allow latching onto either the filter changed or regex changed. also adding comments around the "safe" escaped regex.

--- src/Components/Common/Search/SearchViewModel.ts
+++ src/Components/Common/Search/SearchViewModel.ts
@@ -1,7 +1,11 @@
-import { Observable } from  'rx';
 import * as wx from 'webrx';
 
 import { BaseRoutableViewModel } from '../../React/BaseRoutableViewModel';
+
+export interface SearchRequest {
+  filter: string;
+  regex: RegExp;
+}
 
 export interface SearchRoutingState {
   filter: string;
@@ -11,40 +15,48 @@ export class SearchViewModel extends BaseRoutableViewModel<SearchRoutingState> {
   public static displayName = 'SearchViewModel';
 
   public filter: wx.IObservableProperty<string>;
-  public regex: wx.IObservableReadOnlyProperty<RegExp>;
+  public requests: wx.IObservableReadOnlyProperty<SearchRequest>;
 
-  public search: wx.ICommand<RegExp>;
+  public search: wx.ICommand<any>;
 
-  constructor(public isLiveSearchEnabled = false, private liveSearchTimeout = 250, private isCaseInsensitive = true, isRoutingEnabled = false) {
+  constructor(private liveSearchTimeout = 250, private isCaseInsensitive = true, isRoutingEnabled = false) {
     super(isRoutingEnabled);
 
     this.filter = wx.property('');
 
-    this.regex = wx
-      .whenAny(this.filter, x => x)
-      .debounce(this.isLiveSearchEnabled ? this.liveSearchTimeout : 0)
-      .map(x => this.createRegex(x))
+    // search is executed when a search is to be performed
+    this.search = wx.command();
+
+    this.requests = wx
+      // project search executions into filter values
+      .whenAny(this.search.results, () => this.filter())
+      // then map into a search request with regex
+      .map(filter => (<SearchRequest>{
+        filter,
+        regex: this.createRegex(filter),
+      }))
       .toProperty();
 
-    this.search = wx.asyncCommand((x: RegExp) => {
-      this.notifyChanged();
-
-      return Observable.of(x);
-    });
-
-    this.subscribe(this.results
-      .invokeCommand(this.routingStateChanged)
-    );
-
-    if (this.isLiveSearchEnabled === true) {
-      this.subscribe(this.regex.changed
-        .invokeCommand(this.search)
+    if (this.liveSearchTimeout > 0) {
+      this.subscribe(
+        this.filter.changed
+          // debounce on the live search timeout
+          .debounce(this.liveSearchTimeout)
+          .invokeCommand(this.search)
       );
     }
+
+    this.subscribe(this.search.results
+      .invokeCommand(this.routingStateChanged)
+    );
   }
 
-  public get results() {
-    return this.search.results;
+  public get filterRequests() {
+    return this.requests.changed.map(x => x.filter);
+  }
+
+  public get regexRequests() {
+    return this.requests.changed.map(x => x.regex);
   }
 
   protected createRegex(filter: string) {
@@ -55,6 +67,8 @@ export class SearchViewModel extends BaseRoutableViewModel<SearchRoutingState> {
         regex = new RegExp(filter, this.isCaseInsensitive ? 'i' : null);
       }
       catch (e) {
+        // strip any regex syntax characters from the filter and create a "safe" escaped regex
+        // see: http://stackoverflow.com/a/6300234/2789877
         filter = filter.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
         regex = new RegExp(filter, this.isCaseInsensitive ? 'i' : null);
       }
