2ca260fc8046d181f2f67b8a5c774e29e4dee1d2
Pat Sissons <pat@marinels.com>
2017-02-16 14:53:42 -0800

    adding String.isString extension type guard

--- src/Extensions/String.ts
+++ src/Extensions/String.ts
@@ -2,9 +2,14 @@ import './Object';
 
 declare global {
   interface StringConstructor {
+    isString(value: any): value is string;
     isNullOrEmpty(value: string): boolean;
     stringify(value: any, replacer?: any, space?: string | number): string;
   }
+}
+
+function isString(value: any): value is string {
+  return typeof(value) === 'string' || value instanceof String;
 }
 
 function isNullOrEmpty(value: string) {
@@ -35,5 +40,6 @@ function stringify(value: any, replacer?: any, space: string | number = 2) {
   return result;
 }
 
+String.isString = Object.fallback(String.isString, isString);
 String.isNullOrEmpty = Object.fallback(String.isNullOrEmpty, isNullOrEmpty);
 String.stringify = Object.fallback(String.stringify, stringify);
