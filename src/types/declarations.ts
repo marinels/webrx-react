dc946e18ecf884ab833ad24524e9e398cfae62fd
Pat Sissons <pat@marinels.com>
2017-03-17 13:24:42 -0700

    updating exports to declarations we don't export anything from this file

--- src/types/declarations.ts
+++ src/types/declarations.ts
@@ -1,22 +1,25 @@
 // tslint:disable:no-unused-variable
 
-// webpack defines
 declare global {
+  // webpack defines
   const DEBUG: boolean;
   const PRODUCTION: boolean;
   const TEST: boolean;
   const WEBPACK_DEV_SERVER: boolean;
+
+  // a set of generic object map interfaces
+  // typescript does not currently define these
+  interface StringMap<T> {
+    [ key: string]: T;
+  }
+
+  interface NumberMap<T> {
+    [ key: number]: T;
+  }
+
+  interface Map<T> extends StringMap<T>, NumberMap<T> {
+  }
 }
 
-// a set of generic object map interfaces
-// typescript does not currently define these
-export interface StringMap<T> {
-  [ key: string]: T;
-};
-
-export interface NumberMap<T> {
-  [ key: number]: T;
-};
-
-export interface Map<T> extends StringMap<T>, NumberMap<T> {
-}
+// since there are no exports, we must perform an empty export
+export {}
