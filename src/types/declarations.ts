64cf3904e7f2a68254243fd7e470cee420602450
Pat Sissons <pat@marinels.com>
2017-03-17 13:01:32 -0700

    adding some generic object map interface exports

--- src/types/declarations.ts
+++ src/types/declarations.ts
@@ -8,5 +8,15 @@ declare global {
   const WEBPACK_DEV_SERVER: boolean;
 }
 
-// since there are no exports, we must perform an empty export
-export { };
+// a set of generic object map interfaces
+// typescript does not currently define these
+export interface StringMap<T> {
+  [ key: string]: T;
+};
+
+export interface NumberMap<T> {
+  [ key: number]: T;
+};
+
+export interface Map<T> extends StringMap<T>, NumberMap<T> {
+}
