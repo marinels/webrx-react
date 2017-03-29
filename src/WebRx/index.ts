cef8ccc4326c14cfacf362220fe2aed4a26f4115
Pat Sissons <pat@marinels.com>
2017-03-29 15:32:39 -0700

    fixing an api surface bug also adding some comments

--- src/WebRx/index.ts
+++ src/WebRx/index.ts
@@ -1,10 +1,14 @@
+// export surface types and utility functions
 export * from './Interfaces';
 export * from './Utils';
 
+// export WebRx implementation
 export * from './Property';
 export * from './Command';
 export * from './WhenAny';
 
-export * from './Extensions';
-
+// export webrx compatibility patches
 export * from './Compat';
+
+// import RxJS extensions
+import './Extensions';
