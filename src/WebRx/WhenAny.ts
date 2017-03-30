102a3fdac3033fc5e7ddee8aaba9ddfe5fc1e69b
Pat Sissons <pat@marinels.com>
2017-03-30 15:43:45 -0700

    fixing typing issue with whenAny

--- src/WebRx/WhenAny.ts
+++ src/WebRx/WhenAny.ts
@@ -95,7 +95,7 @@ export function whenAny<TRet, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
 
 export function whenAny(
   ...args: ObservableOrProperty<any>[],
-): Observable<any>;
+): Observable<any[]>;
 
 export function whenAny<TRet>(...args: any[]): Observable<TRet> {
   let selector = args.pop();
