f23b1fc142b9bbc20d82034c8d8c753fa4abccac
Pat Sissons <pat@marinels.com>
2017-05-08 17:04:00 -0700

    adding filterNull extension for arrays

--- src/Extensions/Array.ts
+++ src/Extensions/Array.ts
@@ -5,11 +5,17 @@ import './Object';
 declare global {
   interface Array<T> {
     asEnumerable(): Enumerable<T>;
+    filterNull<T>(this: Array<T | undefined | null>): Array<T>;
   }
 }
 
 function asEnumerable<T>() {
   return Enumerable.fromArray(<T[]>this);
 }
-
 Array.prototype.asEnumerable = Object.fallback(Array.prototype.asEnumerable, asEnumerable);
+
+function filterNull<T>(this: Array<T | undefined | null>) {
+  return this
+    .filter(x => x != null);
+}
+Array.prototype.filterNull = filterNull;
