aaef419782a40e9f0423e8265892f93e2ce722cd
Pat Sissons <pat@marinels.com>
2017-07-24 18:33:06 -0700

    hotfix for NamespacedPortal to correct incorrect imports

--- src/Components/Common/App/NamespacedPortal.ts
+++ src/Components/Common/App/NamespacedPortal.ts
@@ -1,13 +1,10 @@
-import { Portal } from 'react-bootstrap';
-
-declare module 'react-bootstrap' {
-  interface PortalProps {
-    container: any;
-  }
-}
+import * as React from 'react';
+import { PortalProps } from 'react-bootstrap';
+import { Portal } from 'react-overlays';
 
 export function updateDefaultPortalContainer(container: React.ReactInstance | null) {
-  if (container != null && (Portal.defaultProps == null || Portal.defaultProps.container !== container)) {
-    Portal.defaultProps = Object.assign({}, Portal.defaultProps, { container });
+  const portalClass: React.ComponentClass<Portal.PortalProps> = Portal;
+  if (container != null && (portalClass.defaultProps == null || portalClass.defaultProps.container !== container)) {
+    portalClass.defaultProps = Object.assign({}, portalClass.defaultProps, { container });
   }
 }
