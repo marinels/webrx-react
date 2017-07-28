b2b5dc099ef369433a082863aa85a75227163ee8
Pat Sissons <pat@marinels.com>
2017-07-28 14:31:14 -0700

    backporting console logger patch

--- src/Utils/Logging/Adapters/Console.ts
+++ src/Utils/Logging/Adapters/Console.ts
@@ -4,10 +4,24 @@ import { LogLevel, getLevelName, DefaultLogLevel } from '../LogLevel';
 import { DelegateLogManager, DelegateLogger } from './Delegate';
 
 export class ConsoleLogManager extends DelegateLogManager {
+  private static readonly dummyConsole = {
+    log: function() { return; },
+  };
+
   constructor(defaultLevel: LogLevel) {
     super((manager: ConsoleLogManager) => {
       return (logger, level, text, args) => manager.logAction(logger, level, text, args);
     }, defaultLevel);
+
+    this.sanitizeConsoleObject();
+  }
+
+  private sanitizeConsoleObject() {
+    // this is an added protection for IE9 to support console.log even if the
+    // developer tools are not active.
+    if (typeof window !== 'undefined' && window != null && window.console == null) {
+      (<any>window).console = ConsoleLogManager.dummyConsole;
+    }
   }
 
   private getColorStyle(bgColor = 'transparent', color = 'black') {
@@ -15,65 +29,108 @@ export class ConsoleLogManager extends DelegateLogManager {
   }
 
   private getStyles(level: LogLevel) {
-    let styles: string[] = [];
+    const styles: string[] = [];
 
-    if (level >= LogLevel.Fatal) {
-      // do nothing
-    }
-    else if (level >= LogLevel.Error) {
-      // do nothing
-    }
-    else if (level >= LogLevel.Warn) {
-      // do nothing
-    }
-    else if (level >= LogLevel.Info) {
-      styles.push(this.getColorStyle('lightblue'));
-    }
-    else if (level >= LogLevel.Debug) {
-      styles.push(this.getColorStyle('lightcyan'));
-    }
-    else {
-      styles.push(this.getColorStyle());
+    if ((<any>window).chrome != null) {
+      if (level >= LogLevel.Fatal) {
+        // do nothing
+      }
+      else if (level >= LogLevel.Error) {
+        // do nothing
+      }
+      else if (level >= LogLevel.Warn) {
+        // do nothing
+      }
+      else if (level >= LogLevel.Info) {
+        styles.push(this.getColorStyle('lightblue'));
+      }
+      else if (level >= LogLevel.Debug) {
+        styles.push(this.getColorStyle('lightcyan'));
+      }
+      else {
+        styles.push(this.getColorStyle());
+      }
     }
 
     return styles;
   }
 
   private logAction(logger: DelegateLogger, level: LogLevel, text: string, args: any[]) {
-    const styles = this.getStyles(level);
+    try {
+      const styles = this.getStyles(level);
 
-    this.logToConsole(
-      level,
-      `${ styles.length > 0 ? '%c' : '' }[${ moment().format('HH:mm:ss.SSS') }][${ getLevelName(level) }][${ logger.name }] ${ text }`,
-      ...styles,
-    );
+      this.logToConsole(
+        level,
+        `${ styles.length > 0 ? '%c' : '' }[${ moment().format('HH:mm:ss.SSS') }][${ getLevelName(level) }][${ logger.name }] ${ text }`,
+        ...styles,
+      );
 
-    if (args != null && args.length > 0) {
-      args.forEach(x => {
-        this.logToConsole(level, x);
-      });
+      if (args != null && args.length > 0) {
+        args.forEach(x => {
+          this.logToConsole(level, x);
+        });
+      }
+    } catch (e) {
+      // do nothing
     }
   }
 
-  // tslint:disable:no-console
-  private logToConsole(level: LogLevel, message: any, ...formatting: string[]) {
-    if (level >= LogLevel.Error) {
-      console.error(message, ...formatting);
+  private getLogFunction(level: LogLevel): Function {
+    const noop = function() { return; };
+
+    const coerceLogFunction = function(fn: undefined | typeof console.log) {
+      if (fn == null) {
+        fn = console.log;
+      }
+
+      if (fn == null) {
+        return noop;
+      }
+
+      if (fn instanceof Function) {
+        return fn;
+      }
+
+      return function(message?: any, ...optionalParams: any[]) {
+        // try and log the message if we can
+        if (!String.isNullOrEmpty(message)) {
+          fn!(message);
+        }
+      };
+    };
+
+    if (console == null) {
+      return noop;
+    }
+    else if (level >= LogLevel.Error) {
+      return coerceLogFunction(console.error);
     }
     else if (level >= LogLevel.Warn) {
-      console.warn(message, ...formatting);
+      return coerceLogFunction(console.warn);
     }
     else if (level >= LogLevel.Info) {
-      console.info(message, ...formatting);
+      return coerceLogFunction(console.info);
     }
     else if (level >= LogLevel.Debug) {
-      console.debug(message, ...formatting);
+      return coerceLogFunction(console.debug);
     }
     else {
-      console.log(message, ...formatting);
+      return coerceLogFunction(console.log);
     }
   }
-  // tslint:enable:no-console
+
+  public logToConsole(level: LogLevel, message: any, ...formatting: string[]) {
+    try {
+      const fn = this.getLogFunction(level);
+
+      const consoleRef = console === ConsoleLogManager.dummyConsole ? undefined : console;
+
+      fn.apply(consoleRef, [ message ].concat(formatting));
+    }
+    catch (e) {
+      // do nothing
+    }
+  }
 }
 
 export const Default = new ConsoleLogManager(DefaultLogLevel);
