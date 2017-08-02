ce137028e07c1f066cdc642809ca339d80bee640
Pat Sissons <pat@marinels.com>
2017-08-01 20:54:22 -0700

    adding IxJS augmentation to fix bug see: https://github.com/Reactive-Extensions/IxJS/issues/22

--- src/types/augmentations.ts
+++ src/types/augmentations.ts
@@ -1,3 +1,34 @@
 // tslint:disable:no-unused-variable
 
-// currently there are no augmenatations
+import { Enumerable, OrderedEnumerable, Comparer } from 'ix';
+
+// we want to reuse the createOrderedEnumerable that comes with OrderedEnumerable
+interface OrderedEnumerablePrototype<T> extends OrderedEnumerable<T> {
+  createOrderedEnumerable<TKey>(keySelector: (item: T) => TKey, comparer: Comparer<TKey, TKey> | undefined, descending: boolean): OrderedEnumerable<T>;
+}
+
+// reach in and grab the OrderedEnumerable prototype by creating an ordered enumerable
+const orderedEnumerablePrototype: OrderedEnumerablePrototype<any> = (<any>Enumerable)
+  .empty()
+  .orderBy(undefined)
+  .__proto__
+  .constructor
+  .prototype;
+
+function thenBy<T, TKey>(
+  this: OrderedEnumerablePrototype<T>,
+  keySelector: (item: T) => TKey,
+  comparer?: Comparer<TKey, TKey>,
+) {
+  return this.createOrderedEnumerable(keySelector, comparer, false);
+}
+orderedEnumerablePrototype.thenBy = thenBy;
+
+function thenByDescending<T, TKey>(
+  this: OrderedEnumerablePrototype<T>,
+  keySelector: (item: T) => TKey,
+  comparer?: Comparer<TKey, TKey>,
+) {
+  return this.createOrderedEnumerable(keySelector, comparer, true);
+}
+orderedEnumerablePrototype.thenByDescending = thenByDescending;
