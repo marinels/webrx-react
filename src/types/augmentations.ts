482cf670ba82c3bbc2dd2be47cd2756e2498deb0
Pat Sissons <pat@marinels.com>
2017-08-25 09:57:25 -0700

    fixing how we bring in missing ix operators we need to point ix to the es5 modules so that we can reliably augment

--- src/types/augmentations.ts
+++ src/types/augmentations.ts
@@ -1,1 +1,4 @@
 // tslint:disable:no-unused-variable no-shadowed-variable
+
+// import missing ix operators
+import 'ix/add/iterable-operators/orderby';
--- webpack.common.js
+++ webpack.common.js
@@ -12,6 +12,10 @@ module.exports = {
   resolve: {
     extensions: [ '.ts', '.tsx', '.webpack.js', '.web.js', '.js' ],
     alias: {
+      // use es5 target for ix modules
+      'ix': 'ix/targets/es5/cjs',
+      // this ensures we pull in the es5 ix api surface instead of the defautl dist bundle
+      'ix$': 'ix/Ix',
     },
   },
   // failOnError: true,
