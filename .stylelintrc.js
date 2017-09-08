6506b8ebc4d33bfe34964cd9256baaee22f80041
Pat Sissons <pat@marinels.com>
2017-09-08 11:28:43 -0700

    cleaning up stylelint now using stylelint-config-standard with some custom strict rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,23 +1,49 @@
-const config = require('stylelint-config-suitcss');
+module.exports = {
+  'extends': 'stylelint-config-standard',
+  'plugins': [
+    'stylelint-order',
+  ],
+  'rules': {
+    // we want cleaner code appearance, so empty lines between nested styles are required
+    'at-rule-empty-line-before': [ 'always', { 'except': [ 'first-nested' ], 'ignore': [ 'after-comment' ] } ],
 
-config.rules = Object.assign(config.rules, {
-  // standardize on single quotes
-  'string-quotes': [ 'single' ],
+    // do not permit vendor prefix rules
+    'at-rule-no-vendor-prefix': true,
 
-  // Limit the length of a line to 120 chars
-  'max-line-length': [ 120 ],
+    // force quotes around font names
+    'font-family-name-quotes': [ 'always-unless-keyword' ],
 
-  // Limit the specificity of selectors.
-  'selector-max-specificity': [ '0,6,0' ],
+    // vlaues using a url function must be quoted. i.e., background: url('bg.png');
+    'function-url-quotes': 'always',
 
-  // force quotes around font names
-  'font-family-name-quotes': [ 'always-unless-keyword' ],
+    // Limit the length of a line to 120 chars
+    'max-line-length': [ 120, { ignore: 'non-comments' } ],
 
-  // we want cleaner code appearance, so empty lines between nested styles are required
-  'rule-empty-line-before': [ 'always-multi-line', { 'except': [ 'first-nested' ], 'ignore': [ 'after-comment' ] } ],
+    // do not permit vendor prefix rules
+    'media-feature-name-no-vendor-prefix': true,
 
-  // similar to the above rule
-  'at-rule-empty-line-before': [ 'always', { 'except': [ 'after-same-name', 'first-nested' ], 'ignore': [ 'after-comment' ] } ],
-});
+    // do not permit units applied to zero values
+    'length-zero-no-unit': true,
 
-module.exports = config;
+    // ensure all properties are in alphabetical order
+    'order/properties-alphabetical-order': true,
+
+    // do not permit vendor prefix rules
+    'property-no-vendor-prefix': true,
+
+    // similar to the above at-rule-empty-line-before rule
+    'rule-empty-line-before': [ 'always-multi-line', { 'except': [ 'first-nested' ], 'ignore': [ 'after-comment' ] } ],
+
+    // Limit the specificity of selectors (6 layers of nesting max).
+    'selector-max-specificity': [ '0,6,0' ],
+
+    // do not permit vendor prefix rules
+    'selector-no-vendor-prefix': true,
+
+    // standardize on single quotes
+    'string-quotes': [ 'single' ],
+
+    // do not permit vendor prefix rules
+    'value-no-vendor-prefix': true,
+  }
+}
