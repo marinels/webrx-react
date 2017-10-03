bff41204455c9b5ff40b8bc4f7c44c8b98fa0a22
Pat Sissons <pat@marinels.com>
2017-10-02 17:40:18 -0700

    adding some comments to the api surface

--- src/webrx-react.ts
+++ src/webrx-react.ts
@@ -3,16 +3,22 @@
 // import all custom styles
 import './Style/App.less';
 
+// apply augmentations
 import './types/augmentations';
 
+// import extensions
 import './Extensions';
 
+// import WebRx
 import * as wx from './WebRx';
+
+// import webrx-react submodules
 import { Logging, PubSub, Alert, Compare, Moment } from './Utils';
 import * as Events from './Events';
 import * as Routing from './Routing';
 import * as Stores from './Stores';
 import * as Components from './Components';
 
+// export everything
 export { wx, Logging, PubSub, Alert, Compare, Moment };
 export { Events, Routing, Stores, Components };
