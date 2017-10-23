8443f49cb5437ebd85f8acad645f2a67054a72bc
Pat Sissons <pat@marinels.com>
2017-10-23 12:34:12 -0700

    emitting a console debug line with version details for diagnostics

--- src/webrx-react.ts
+++ src/webrx-react.ts
@@ -28,3 +28,7 @@ import * as Components from './Components';
 // export everything
 export { wx, Logging, PubSub, Alert, Compare, Moment };
 export { Events, Routing, Stores, Components };
+
+// inform the user which version we are using
+// tslint:disable-next-line:no-console
+console.debug(`Loaded webrx-react: ${ version }`);
