ecdba3bf67dbeeb64bc607e3b384674d1f2cdd16
Pat Sissons <pat@marinels.com>
2017-11-03 14:49:19 -0700

    adding ixjs patch for broken single predicate param signature

--- src/WebRx/Extensions.ts
+++ src/WebRx/Extensions.ts
@@ -124,6 +124,13 @@ declare module 'ix/iterable/iterablex' {
   interface IterableX<T> {
     filterNull: typeof filterNullIterable;
   }
+
+  // NOTE: temporary patch to deal with IxJS issue
+  // see: https://github.com/ReactiveX/IxJS/pull/83#issuecomment-341832266
+  function singleProto<T>(this: IterableX<T>, predicate?: (value: T, index: number) => boolean): T | undefined;
+  interface IterableX<T> {
+    single: typeof singleProto;
+  }
 }
 
 declare module 'rxjs/Subscription' {
