e62a21391e79cc8b8edce1aab4f1a062b60c590c
Pat Sissons <pat@marinels.com>
2017-11-03 17:27:39 -0700

    removing old modal patch that no longer functions

--- src/types/augmentations.ts
+++ src/types/augmentations.ts
@@ -2,18 +2,3 @@
 
 // include additional imports
 import './imports';
-
-// this is a patch for a bug in react-overlays that prevents modals from functioning properly
-// see: https://github.com/react-bootstrap/react-overlays/issues/188
-import { Modal } from 'react-overlays';
-Modal.prototype.componentWillMount = function() {
-  this.setModalNode = function(ref: any) {
-    this.modalNode = ref;
-    if (ref != null && this.props.show) {
-      this.onShow();
-    }
-  }.bind(this);
-};
-Modal.prototype.componentDidMount = function() {
-  this._isMounted = true;
-};
