907f96bb2985adadb00700e4b4e67cf6f0849fa2
Pat Sissons <pat@marinels.com>
2017-11-07 14:43:12 -0800

    search now has a visual queue for pending search status this queue is visible until the search request is generated resolves #176

--- src/Components/Common/Search/SearchView.tsx
+++ src/Components/Common/Search/SearchView.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Observable } from 'rxjs';
-import { Icon } from 'react-fa';
+import { Icon, IconStack } from 'react-fa';
 import { FormGroup, InputGroup, FormControl } from 'react-bootstrap';
 
 import { BaseView, BaseViewProps } from '../../React';
@@ -24,6 +24,12 @@ export class SearchView extends BaseView<SearchViewProps, SearchViewModel> {
     placeholder: 'Search',
   };
 
+  updateOn() {
+    return [
+      this.viewModel.searchPending.changed,
+    ];
+  }
+
   render() {
     const { className, rest } = this.restProps(x => {
       const { placeholder } = x;
@@ -32,13 +38,31 @@ export class SearchView extends BaseView<SearchViewProps, SearchViewModel> {
 
     return (
       <div { ...rest } className={ this.classNames('Search', className) }>
-        <FormGroup className='has-feedback'>
+        <FormGroup>
           <BindableInput boundProperty={ this.viewModel.filter }>
             <FormControl className='Search-text' type='text' placeholder={ this.props.placeholder }
               onKeyDown={ this.bindEventToCommand(x => x.search, undefined, (e: React.KeyboardEvent<any>) => e.keyCode === EnterKey) }
             />
           </BindableInput>
-          <Icon className='Search-icon form-control-feedback' name='search' />
+          {
+            this.renderConditional(
+              this.viewModel.searchPending,
+              () => (
+                <Icon className='Search-icon Search-pendingIcon' name='spinner' pulse />
+              ),
+              () => this.renderConditional(
+                String.isNullOrEmpty(this.viewModel.filter.value) === false,
+                () => (
+                  <CommandButton className='Search-icon Search-clearIcon' plain command={ this.viewModel.clear }>
+                    <Icon name='times' />
+                  </CommandButton>
+                ),
+              ),
+            )
+          }
+          <CommandButton className='Search-icon' plain command={ this.viewModel.search }>
+            <Icon name='search' />
+          </CommandButton>
         </FormGroup>
       </div>
     );
