aaf8212443a8ef5dc88542a4658ed3b02a22f2e3
Pat Sissons <pat@marinels.com>
2017-11-10 13:40:58 -0800

    updating routing types to support required state object

--- src/Routing/Interfaces.ts
+++ src/Routing/Interfaces.ts
@@ -3,12 +3,12 @@ import { Observable, Scheduler } from  'rxjs';
 export interface Route {
   path: string;
   params?: string;
-  state?: any;
+  state: { route?: Route };
   match?: RegExpMatchArray;
 }
 
 export interface HashManager {
   readonly hashChanged: Observable<string>;
   // hash will contain the # prefix
-  updateHash: (hash: string, state: any, title: string, replace: boolean) => void;
+  updateHash: (hash: string, state: {}, title: string, replace: boolean) => void;
 }
