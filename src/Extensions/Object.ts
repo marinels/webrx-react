596db820c11df42cf1e1ec9177a36cd316a74a7d
Pat Sissons <pat@marinels.com>
2017-11-10 09:56:08 -0800

    removing Partial<T> return time from Object.trim as we are not changing the type, only removing optional members.

--- src/Extensions/Object.ts
+++ src/Extensions/Object.ts
@@ -4,7 +4,7 @@ export function isObject(value: any): value is {} {
   return typeof value === 'object' || value instanceof Object;
 }
 
-export function trim<T extends {}>(obj: T, trimNull = true): Partial<T> {
+export function trim<T extends {}>(obj: T, trimNull = true): T {
   if (isObject(obj)) {
     return Iterable
       .from(Object.keys(obj))
