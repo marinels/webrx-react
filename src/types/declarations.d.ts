d1a694ab696d14d798e5b3a34f016c277d2eb85a
Pat Sissons <pat@marinels.com>
2017-11-10 09:54:40 -0800

    expanding Diff and Omit type declarations to support two exclusions there are a few cases where we do two exclusions and it's much cleaner like this

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -17,13 +17,22 @@ declare global {
   }
 
   // some type constructs to support type outersection (i.e., type subtraction)
-  type Diff<T extends string, U extends string> = (
+  type Diff<T extends string, U1 extends string> = (
     & { [ P in T ]: P }
-    & { [ P in U ]: never }
+    & { [ P in U1 ]: never }
+    & { [ x: string ]: never }
+  )[T];
+
+  type Diff2<T extends string, U1 extends string, U2 extends string> = (
+    & { [ P in T ]: P }
+    & { [ P in U1 ]: never }
+    & { [ P in U2 ]: never }
     & { [ x: string ]: never }
   )[T];
 
   type Omit<T, TExclude> = Pick<T, Diff<keyof T, keyof TExclude>>;
+
+  type Omit2<T, TExclude1, TExclude2> = Pick<T, Diff2<keyof T, keyof TExclude1, keyof TExclude2>>;
 }
 
 export {};
