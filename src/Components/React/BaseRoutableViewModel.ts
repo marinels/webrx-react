9694091fc5687f9117dc34ace0334b0146cc6feb
Pat Sissons <pat@marinels.com>
2017-12-06 17:37:34 -0800

    shuffling how we manage routing state to the route handler we can't rely on routable components to do this job unless we enforce constraints on no routable sub components which is not reasonable.

--- src/Components/React/BaseRoutableViewModel.ts
+++ src/Components/React/BaseRoutableViewModel.ts
@@ -4,7 +4,6 @@ import { ReadOnlyProperty, Property, Command } from '../../WebRx';
 import { BaseViewModel } from './BaseViewModel';
 import { HeaderCommandAction, HeaderMenu } from './Actions';
 import { PubSub } from '../../Utils';
-import { RoutingStateChangedKey } from '../../Events';
 import { HandlerRoutingStateChanged, RoutingStateHandler, Search } from './Interfaces';
 
 export function isRoutableViewModel(value: any): value is BaseRoutableViewModel<any> {
@@ -38,7 +37,7 @@ export abstract class BaseRoutableViewModel<T> extends BaseViewModel implements 
   public readonly documentTitle: ReadOnlyProperty<string>;
   public readonly breadcrumbs: ReadOnlyProperty<Array<RoutingBreadcrumb> | undefined>;
 
-  constructor(routingStateRateLimit = 25) {
+  constructor() {
     super();
 
     this.updateDocumentTitle = this.wx.command((title: any) => (title || '').toString());
@@ -48,26 +47,6 @@ export abstract class BaseRoutableViewModel<T> extends BaseViewModel implements 
     this.breadcrumbs = this.wx
       .whenAny(this.updateRoutingBreadcrumbs.results, x => x)
       .toProperty();
-
-    this.addSubscription(
-      PubSub
-        .observe<HandlerRoutingStateChanged>(RoutingStateChangedKey)
-        .debounceTime(routingStateRateLimit)
-        .map(change => this.createRoutingState(change))
-        .filterNull()
-        .subscribe(
-          (state: T) => {
-            this.navToState(state);
-          },
-          error => {
-            this.alertForError(error, 'Routing State Changed Error');
-          },
-        ),
-    );
-  }
-
-  protected navToState(state: {}, uriEncode = false) {
-    this.navTo(this.routeManager.currentRoute.value.path, state, true, uriEncode);
   }
 
   isRoutingStateHandler() {
